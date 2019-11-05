a2d072e9b7cc7df11a90eea7d6a05810645b0634
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-11-05 13:27:52 +0530

    add recog

--- ksigmasample/krekognizer.js
+++ ksigmasample/krekognizer.js
@@ -1,4 +1,20 @@
-exports.handler = function(event, context, callback) {
+const AWS = require('aws-sdk');
+const rekog = new AWS.Rekognition();
 
-    callback(null, {"message": "Successfully executed"});
+exports.handler = function (event, context, callback) {
+    let s3 = event.Records[0].s3;
+    rekog.detectLabels({
+        Image: {
+            S3Object: {
+                Bucket: s3.bucket.name,
+                Name: s3.object.key
+            }
+        },
+        MaxLabels: 1
+    }).promise()
+        .then(data => {
+            console.log(data);
+            callback(null, {});
+        })
+        .catch(callback);
 }
